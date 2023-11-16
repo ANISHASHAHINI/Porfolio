59f63fa39ee4f16fcbab5ed045d5d60107cd75bb
ANISHASHAHINI <anishashahini2002@gmail.com>
2023-11-17 01:39:02 +0530

    commit

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,19 +6,26 @@ import HesderSocials from './HesderSocials'
 
 const Header = () => {
   return (
-    <header id='home'>
-      <div className="container header__container">
-        <h5>Hello I'm</h5>
-        <h2>Anisha shahini</h2>
-        <h5 className="text-light">Full stack developer</h5>
-        <CaTA />
-        <HesderSocials />
-        <div className='me'>
-          <img src= {ME} alt="me" />
+      <header>
+         <h1>welcome!</h1>
+        <h5>"Embarking on my journey as a budding developer, I invite you to witness the evolution of my coding 
+          odyssey. Eager to contribute, ready to learn, and passionate about creating meaningful digital experiences.
+           Join me on this exciting expedition where curiosity meets code, and every challenge is an opportunity to 
+           grow. Let's build a future together, one line of code at a time."</h5>
+        <section id='home'>
+        <div className="container header__container">
+          <h5>Hello I'm</h5>
+          <h2>Anisha shahini</h2>
+          <h5 className="text-light">Full stack developer</h5>
+          <CaTA />
+          <HesderSocials />
+          <div className='me'>
+            <img src= {ME} alt="me" />
+          </div>
+          <a href="#contact" className='scroll__down'>Scroll Down</a>
         </div>
-        <a href="#contact" className='scroll__down'>Scroll Down</a>
-      </div>
-    </header>
+      </section>
+      </header>
   )
 }
 
